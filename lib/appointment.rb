8120bc0868942543895153898f2240a30774c0ac
Marc <marc.ferraro1@gmail.com>
2020-12-17 15:31:46 -0600

    commit tree 1a0517c145b45d1ee74292e3d14fc7fb35b0dc86

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,20 @@
+class Appointment
+    def initialize(date, patient_instance, doctor_instance)
+        @date = date
+        @patient = patient_instance
+        @doctor = doctor_instance
+        @@all << self
+    end
 
+    @@all = []
+
+    attr_reader :date, :patient, :doctor
+    
+    def self.all
+        @@all
+    end
+
+    def new
+        
+    end
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,28 @@
+class Patient
+    def initialize(name)
+        @date = name
+        @@all << self
+    end
 
+    @@all = []
+
+    attr_reader :name
+    
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+        # new_appointment(date, patient)
+        # Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select {|apt| apt.patient == self}
+    end
+
+    def doctors
+        self.appointments.map {|apt| apt.doctor}
+    end
+end
